624304dd4cdd1eb93fd67744546a8dd438058a02
priya <ashishashok893@gmail.com>
2024-07-08 15:24:13 +0530

    updated version 1

---
+++ Square_Root.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class Square_Root {
+
+    public static void main(String[] args) throws IOException {
+
+        Scanner o = new Scanner(System.in);
+
+        System.out.println("Enter a number to find the number of square root: ");
+
+
+
+        System.out.println(Math.sqrt(o.nextInt()));
+    }
+}
