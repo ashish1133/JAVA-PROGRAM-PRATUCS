1902b96a39f0affa9f79f50da6ddd8fa03c98f4f
ASHISH <105844161+ashish1133@users.noreply.github.com>
2024-08-06 20:17:26 +0530

    Searching Node

--- Binary_Search_Tree.java
+++ Binary_Search_Tree.java
@@ -3,33 +3,44 @@ class Node{
     Node left,right;
     public Node(int item){
         key=item;
-        left=right=null;
+        left = right = null;
     }
 }
-
-public class Binary_Search_Tree {
-
+class BST{
     static Node search(Node root,int key){
-        if (root.key==key)
-            return root;
 
-        if (root.key<key)
-            return root.right;
-
-        return root.left;
+        while (root!=null){
+            if (key>root.key){
+                root=root.right;
+            }
+            else if(key<root.key){
+                root=root.left ;
+            }
+            else {
+                return root;
+            }
+        }
+        return null;
     }
-
+    // Driver Code
     public static void main(String[] args) {
 
-        Node root= new Node(5);
-        root.left = new Node(3);
-        root.right= new Node(7);
-        root.left.left = new Node(2);
-        root.left.right=new Node(4);
-        root.right.left=new Node(6);
-        root.right.right=new Node(8);
+        // Creating a hardcoded tree for keeping
+        // the length of the code small. We need
+        // to make sure that BST properties are
+        // maintained if we try some other cases.
+        Node root = new Node(50);
+        root.left = new Node(30);
+        root.right = new Node(70);
+        root.left.left = new Node(20);
+        root.left.right = new Node(40);
+        root.right.left = new Node(60);
+        root.right.right = new Node(80);
 
-        System.out.println(search(root,3)!=null?"Found the Node":"Not found the Node");
-        System.out.println(search(root,8)!=null?"Found the Node":"Not found the Node");
+        System.out.println(search(root, 19) != null ?
+                "Found" : "Not Found");
+        System.out.println(search(root, 80) != null ?
+                "Found" : "Not Found");
     }
+
 }
