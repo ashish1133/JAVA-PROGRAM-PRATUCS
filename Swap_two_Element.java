3bfe2a9ff099b6e7a11fc41ac273560e685f8188
ASHISH <105844161+ashish1133@users.noreply.github.com>
2024-08-13 17:38:59 +0530

    Swappeing two element

---
+++ Swap_two_Element.java
@@ -0,0 +1,15 @@
+public class Swap_two_Element {
+
+    public static void main(String[] args) {
+
+        String A = "Ashish";
+        String B = "Ashok";
+
+        String temp = A;
+        A = B;
+        B = temp;
+
+        System.out.println("A "+A);
+        System.out.println("B "+B);
+    }
+}
