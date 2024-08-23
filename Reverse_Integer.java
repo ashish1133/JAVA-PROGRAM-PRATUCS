c279f53f42a8a925b1c6787eae24c49a09bbb82d
ASHISH <105844161+ashish1133@users.noreply.github.com>
2024-08-24 01:09:01 +0530

    Reverse_Integer

---
+++ Reverse_Integer.java
@@ -0,0 +1,20 @@
+public class Reverse_Integer {
+
+    public static void main(String[] args) {
+
+        int k= 123;
+
+        int sum=0,digit;
+        k=Math.abs(k);
+        while (k>0){
+
+            digit = k%10;
+            sum = (sum*10) + digit;
+            k = k/10;
+
+        }
+        System.out.println(sum);
+
+    }
+
+}
