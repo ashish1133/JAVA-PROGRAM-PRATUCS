6b5b9900631761f3b0b345a4ce18786e4a6a0e79
priya <ashishashok893@gmail.com>
2024-08-28 20:44:40 +0530

    Arrays

---
+++ Arrays/Find_positive_interger_in_Array.java
@@ -0,0 +1,12 @@
+class main{
+    public static void main(String[] args) {
+
+        int[]arr={-1,2,-4,3,5,-2,0};
+        for (int i=0;i< arr.lengthli++){
+            if (arr[i]>0){
+                System.out.print(arr[i]+" ");
+            }
+        }
+
+    }
+}
