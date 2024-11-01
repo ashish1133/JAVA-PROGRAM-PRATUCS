ced4a7b09c06a57f9093e62e55d36eae07e9b40c
priya <ashishashok893@gmail.com>
2024-11-01 14:05:00 +0530

    Digit_to_Word

---
+++ String/digit_Converted_Word.java
@@ -0,0 +1,25 @@
+class main{
+    static String[]word ={"zero","one","two","three","four","five","six","seven","eight","nine"};
+
+    static String digitoword (int num){
+
+        String k = Integer.toString(num);
+        StringBuilder result = new StringBuilder();
+
+        for (char digit : k.toCharArray()){
+
+            int digitValue = digit-'0';
+            result.append(word[digitValue]);
+        }
+        return result.toString();
+    }
+
+    public static void main(String[] args) {
+
+        int num = 312;
+        String result = digitoword(num);
+        System.out.println("Digit to Word : "+result);
+
+    }
+
+}
