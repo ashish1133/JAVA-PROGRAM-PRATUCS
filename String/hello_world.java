496e480e08d73b5f2c88d3fd74f6c18d13bf9d0f
ASHISH <105844161+ashish1133@users.noreply.github.com>
2025-07-22 03:35:16 +0530

    Update hello_world.java

--- String/hello_world.java
+++ String/hello_world.java
@@ -3,8 +3,8 @@ package String;
 public class hello_world {
     public static void main(String[] args) {
 
-        System.out.println("Hello World! ");
-        System.out.println("Leraning in java ");
+        System.out.println(" Hello World! ");
+        System.out.println(" Leraning in java ");
 
     }
 }
