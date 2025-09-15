a2546a343da75fb7e451eeda07beecc75564d1f1
ASHISH <105844161+ashish1133@users.noreply.github.com>
2025-09-15 19:31:40 +0530

    update

--- String/hello_world.java
+++ String/hello_world.java
@@ -4,7 +4,7 @@ public class hello_world {
     public static void main(String[] args) {
 
         System.out.println(" Hello World! ");
-        System.out.println(" Leraning in java ");
+        
 
     }
 }
